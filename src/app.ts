a03d2a9bad01c470e821d6d288492f6682a78b85
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2025-07-16 21:15:00 +0530

    Add Cloudflare scheduler function

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 // src/app.ts
 import "dotenv/config";
 import "./scheduler";
+import { runDigestJob } from "./scheduler";
 
 console.log("X Digest Bot is up and running...");
+
+export default {
+  async scheduled(event: ScheduledEvent, env: Env, ctx: ExecutionContext): Promise<void> {
+    runDigestJob();
+  }
+};
