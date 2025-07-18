eb8a53c7a999506f70ce26eaa7807e2c8975f6be
Zilu Ramkrishna Rane <zilurrane@gmail.com>
2025-07-18 21:04:17 +0530

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,12 @@
 // src/app.ts
 import "dotenv/config";
 import "./scheduler";
+import { runDigestJob } from "./scheduler";
 
 console.log("X Digest Bot is up and running...");
+
+export default {
+  async scheduled(event: any, env: any, ctx: any): Promise<void> {
+    runDigestJob();
+  }
+};
